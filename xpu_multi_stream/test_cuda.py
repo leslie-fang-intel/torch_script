835e30e18705bafbab0c431c49345ff9a627ed3d
Jiang, Xiaofei <xiaofei.jiang@intel.com>
2022-11-01 13:27:03 +0800

    add torch no_grad

--- xpu_multi_stream/test_cuda.py
+++ xpu_multi_stream/test_cuda.py
@@ -6,11 +6,11 @@ import time
 
 USE_CUDA = True
 
-CUDA_ONLY = False
+CUDA_ONLY = True
 CPU_ONLY = False
-USE_XPU = True
+USE_XPU = False
 
-CUDA_TASK = True
+CUDA_TASK = False
 
 def measurement_performance():
     bs_cpu = 14
@@ -21,45 +21,51 @@ def measurement_performance():
 
     x_cpu = x[0:bs_cpu]
     # **TODO** copy.deepcopy may fail for some models
-    model_cpu = copy.deepcopy(model)
+    model_cpu = copy.deepcopy(model).eval()
 
     if USE_CUDA:
         x_gpu = x[bs_cpu:global_bs].to(device="cuda")
-        model_gpu = model.to(device="cuda")
+        model_gpu = model.to(device="cuda").eval()
         if CUDA_TASK:
             cpu_pool = ipex.cpu.runtime.CPUPool(core_ids=[0,])
             #cpu_pool = ipex.cpu.runtime.CPUPool(node_id=0)
             model_gpu = ipex.cpu.runtime.Task(model_gpu, cpu_pool)
 
-    warm_up_iterations = 20
-    for i in range(warm_up_iterations):
-        if CUDA_ONLY or USE_XPU:
-            res_gpu = model_gpu(x_gpu)
-        if CPU_ONLY or USE_XPU:
-            res_cpu = model_cpu(x_cpu)
-        if CUDA_TASK:
-            res_gpu = res_gpu.get()
+    print("x_gpu size: {}".format(x_gpu.size()))
 
-    iterations = 100
-    start = time.time()
-    for i in range(iterations):
-        if CUDA_ONLY or USE_XPU:
-            res_gpu = model_gpu(x_gpu)
-        if CPU_ONLY or USE_XPU:
-            res_cpu = model_cpu(x_cpu)
-        if CUDA_TASK:
-            res_gpu = res_gpu.get()
-    time_cost = (time.time() - start)
+    with torch.no_grad():
+        warm_up_iterations = 20
+        for i in range(warm_up_iterations):
+            if CUDA_ONLY or USE_XPU:
+                res_gpu = model_gpu(x_gpu)
+            if CPU_ONLY or USE_XPU:
+                res_cpu = model_cpu(x_cpu)
+            if CUDA_TASK:
+                res_gpu = res_gpu.get()
 
-    print("time_cost: {}".format(time_cost))
-    if CUDA_ONLY:
-        throughtput = bs_gpu * iterations / time_cost
-    elif CPU_ONLY:
-        throughtput = bs_cpu * iterations / time_cost
-    else:
-        throughtput = (bs_cpu + bs_gpu) * iterations / time_cost
-    print("throughtput: {}".format(throughtput))
-    return
+        print("Finish the warm up step")
+
+        iterations = 50
+        start = time.time()
+        for i in range(iterations):
+            if CUDA_ONLY or USE_XPU:
+                res_gpu = model_gpu(x_gpu)
+            if CPU_ONLY or USE_XPU:
+                res_cpu = model_cpu(x_cpu)
+            if CUDA_TASK:
+                res_gpu = res_gpu.get()
+            print("Finish the formal step: {}".format(i))
+        time_cost = (time.time() - start)
+
+        print("time_cost: {}".format(time_cost))
+        if CUDA_ONLY:
+            throughtput = bs_gpu * iterations / time_cost
+        elif CPU_ONLY:
+            throughtput = bs_cpu * iterations / time_cost
+        else:
+            throughtput = (bs_cpu + bs_gpu) * iterations / time_cost
+        print("throughtput: {}".format(throughtput))
+        return
 
 def test_cuda_task():
     x = torch.randn(1000, 3, 224, 224).contiguous(memory_format=torch.channels_last)
