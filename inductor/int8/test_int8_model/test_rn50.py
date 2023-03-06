fb676df9795dfc803e7f847a4f8fe8188c88f3ce
leslie-fang-intel <leslie.fang@intel.com>
2023-03-06 17:31:25 +0800

    add torch no grad

--- inductor/int8/test_int8_model/test_rn50.py
+++ inductor/int8/test_int8_model/test_rn50.py
@@ -21,29 +21,29 @@ def test_fp32():
         aten_graph=True,
         tracing_mode=tracing_mode,
     )
+    with torch.no_grad():
+        base_res = m(*example_inputs)
+        base_res = m(*example_inputs)
+        print("base_res.size() is: {}".format(base_res.size()), flush=True)
+        input2 = torch.randn(200, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+        base_res2 = m(input2)
+        print("base_res2.size() is: {}".format(base_res2.size()), flush=True)
+        input3 = torch.randn(2, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+        base_res3 = m(input3)
+        print("base_res3.size() is: {}".format(base_res3.size()), flush=True)
 
-    base_res = m(*example_inputs)
-    base_res = m(*example_inputs)
-    print("base_res.size() is: {}".format(base_res.size()), flush=True)
-    input2 = torch.randn(200, 3, 224, 224).contiguous(memory_format=torch.channels_last)
-    base_res2 = m(input2)
-    print("base_res2.size() is: {}".format(base_res2.size()), flush=True)
-    input3 = torch.randn(2, 3, 224, 224).contiguous(memory_format=torch.channels_last)
-    base_res3 = m(input3)
-    print("base_res3.size() is: {}".format(base_res3.size()), flush=True)
 
+        run = compile_fx(m, example_inputs)
 
-    run = compile_fx(m, example_inputs)
-
-    inductor_res = run(*example_inputs)
-    inductor_res = run(*example_inputs)
-    print("inductor_res.size() is: {}".format(inductor_res.size()), flush=True)
-    input2 = torch.randn(200, 3, 224, 224).contiguous(memory_format=torch.channels_last)
-    inductor_res2 = run(input2)
-    print("inductor_res2.size() is: {}".format(inductor_res2.size()), flush=True)
-    input3 = torch.randn(2, 3, 224, 224).contiguous(memory_format=torch.channels_last)
-    inductor_res3 = run(input3)
-    print("inductor_res3.size() is: {}".format(inductor_res3.size()), flush=True)
+        inductor_res = run(*example_inputs)
+        inductor_res = run(*example_inputs)
+        print("inductor_res.size() is: {}".format(inductor_res.size()), flush=True)
+        input2 = torch.randn(200, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+        inductor_res2 = run(input2)
+        print("inductor_res2.size() is: {}".format(inductor_res2.size()), flush=True)
+        input3 = torch.randn(2, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+        inductor_res3 = run(input3)
+        print("inductor_res3.size() is: {}".format(inductor_res3.size()), flush=True)
 
 
 if __name__ == "__main__":
