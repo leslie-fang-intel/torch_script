1a3e655374c38156da4bdffb0259ea2e59124377
leslie-fang-intel <leslie.fang@intel.com>
2023-03-09 09:09:24 +0800

    add test case for recipe

---
+++ inductor/int8/test_int8_model/test_int8_conv_add_relu_recipe.py
@@ -0,0 +1,171 @@
+import torch
+import torch._dynamo as torchdynamo
+from torch.ao.quantization import (
+    get_default_qconfig,
+    QConfigMapping,
+)
+from torch.ao.quantization._quantize_pt2e import prepare_pt2e, convert_pt2e
+
+class Mod(torch.nn.Module):
+    def __init__(self,) -> None:
+        super().__init__()
+        self.conv = torch.nn.Conv2d(
+            in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.maxpool = torch.nn.MaxPool2d(2)
+        self.conv2 = torch.nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv3 = torch.nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1, bias=True)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        x1 = self.conv(x)
+        #x1 = self.maxpool(self.conv(x))
+        return self.relu(self.conv2(x1) + self.conv3(x1))
+
+def test_expected():
+    import copy
+    from torch.ao.quantization.backend_config._inductor_pt2e import get_inductor_pt2e_backend_config
+    from torch._inductor.compile_fx import compile_fx, compile_fx_quantization
+    torch.backends.quantized.engine = "x86"
+    example_inputs = (torch.randn(1, 3, 16, 16),)
+    # m = Mod().eval()
+    
+    import torchvision.models as models
+    m = models.__dict__["resnet50"](pretrained=True).eval()
+
+    m, guards = torchdynamo.export(
+        m,
+        *copy.deepcopy(example_inputs),
+        aten_graph=True,
+        tracing_mode="real",
+    )
+    m = m.eval()
+    print("model after torchdynamo export is: {}".format(m), flush=True)
+
+    backend_config = get_inductor_pt2e_backend_config()
+    qconfig = get_default_qconfig("x86")
+    qconfig_mapping = QConfigMapping().set_global(qconfig)
+    before_fusion_result = m(*example_inputs)
+
+    m = prepare_pt2e(m, qconfig_mapping, example_inputs, backend_config)
+    after_prepare_result = m(*example_inputs)
+    print("model after prepare_pt2e is: {}".format(m), flush=True)
+
+    # Draw Graph
+    from torch.fx.passes.graph_drawer import FxGraphDrawer
+    g = FxGraphDrawer(m, "resnet50")
+    g.get_dot_graph().write_svg("expected_prepare.svg")
+
+    print("check the result after prepare: {}".format(
+        torch.allclose(before_fusion_result, after_prepare_result)),
+        flush=True)
+    m = convert_pt2e(m)
+    after_quant_result = m(*example_inputs)
+    print("model after convert_pt2e is: {}".format(m), flush=True)
+    print("check the result after convert: {}".format(
+        torch.allclose(before_fusion_result, after_quant_result, rtol=5e-02, atol=5e-02)),
+        flush=True)
+    run = compile_fx_quantization(m, example_inputs)
+    # first run
+    print("start the first run", flush=True)
+    inductor_result = run(*example_inputs)
+
+    # second run
+    print("start the second run", flush=True)
+    inductor_result = run(*example_inputs)
+    print(torch.allclose(inductor_result[0], after_quant_result, rtol=1e-01, atol=1e-01), flush=True)
+    print("Finish the test", flush=True)
+
+def test():
+    from torch.ao.quantization.backend_config import (
+        BackendConfig,
+        DTypeConfig,
+        ObservationType,
+        BackendPatternConfig,
+    )
+    from torch.ao.quantization.utils import MatchAllNode
+    import itertools
+    import copy
+    weighted_op_quint8_dtype_config = DTypeConfig(
+        input_dtype=torch.quint8,
+        output_dtype=torch.quint8,
+        weight_dtype=torch.qint8,
+        bias_dtype=torch.float,
+    )
+    def get_conv_configs():
+        conv_configs = []
+        observation_type = ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT
+        dtype_configs = [weighted_op_quint8_dtype_config]
+        conv_configs.append(
+            BackendPatternConfig(torch.ops.aten.convolution.default)
+            .set_observation_type(observation_type)  # noqa: E131
+            .set_dtype_configs(dtype_configs)
+            ._set_input_type_to_index({"weight": 1, "bias": 2})
+        )
+        # Conv add ReLU case
+        def _conv_add_relu_root_node_getter_left(pattern):
+            relu, add_pattern = pattern
+            _, conv, _ = add_pattern
+            return conv
+        def _conv_add_relu_extra_inputs_getter_left(pattern):
+            """ get inputs pattern for extra inputs, inputs for root node
+            are assumed to be copied over from root node to the fused node
+            """
+            relu, add_pattern = pattern
+            _, conv, extra_input = add_pattern
+            return [extra_input]
+        
+        conv_add_relu_optioins = itertools.product(
+            [torch.ops.aten.add.Tensor, torch.ops.aten.add_.Tensor],  # add op
+            [torch.ops.aten.relu.default, torch.ops.aten.relu_.default],  # relu op
+        )
+        for add_op, relu_op in conv_add_relu_optioins:
+            conv_configs.append(
+                BackendPatternConfig()
+                    ._set_pattern_complex_format((relu_op, (add_op, torch.ops.aten.convolution.default, MatchAllNode)))  # noqa: E131
+                    .set_observation_type(observation_type)
+                    .set_dtype_configs(dtype_configs)
+                    ._set_input_type_to_index({"weight": 1, "bias": 2})
+                    ._set_root_node_getter(_conv_add_relu_root_node_getter_left)
+                    ._set_extra_inputs_getter(_conv_add_relu_extra_inputs_getter_left)
+            )
+        return conv_configs
+
+    def get_inductor_pt2e_backend_config():
+        return (
+            BackendConfig("inductor_pytorch_2.0_export")
+            .set_backend_pattern_configs(get_conv_configs())
+        )
+    torch.backends.quantized.engine = "x86"
+    example_inputs = (torch.randn(1, 3, 16, 16),)
+    m = Mod().eval()
+
+    # import torchvision.models as models
+    # m = models.__dict__["resnet50"](pretrained=True).eval()
+    
+    m, guards = torchdynamo.export(
+        m,
+        *copy.deepcopy(example_inputs),
+        aten_graph=True,
+        tracing_mode="real",
+    )
+    m = m.eval()
+    print("model after torchdynamo export is: {}".format(m), flush=True)
+
+    backend_config = get_inductor_pt2e_backend_config()
+    qconfig = get_default_qconfig("x86")
+    qconfig_mapping = QConfigMapping().set_global(qconfig)
+    before_fusion_result = m(*example_inputs)
+
+    m = prepare_pt2e(m, qconfig_mapping, example_inputs, backend_config)
+    after_prepare_result = m(*example_inputs)
+    print("model after prepare_pt2e is: {}".format(m), flush=True)
+
+    from torch.fx.passes.graph_drawer import FxGraphDrawer
+    g = FxGraphDrawer(m, "resnet50")
+    g.get_dot_graph().write_svg("prepare_rn50_graph.svg")
+
+if __name__ == "__main__":
+    #test_expected()
+    test()
+    # test_torch_compile()
--- inductor/int8/test_int8_model/test_rn50.py
+++ inductor/int8/test_int8_model/test_rn50.py
@@ -21,6 +21,7 @@ def test_fp32():
         aten_graph=True,
         tracing_mode=tracing_mode,
     )
+    print("model after dynamoexport is: {}".format(m), flush=True)
     with torch.no_grad():
         base_res = m(*example_inputs)
         base_res = m(*example_inputs)
@@ -32,9 +33,24 @@ def test_fp32():
         base_res3 = m(input3)
         print("base_res3.size() is: {}".format(base_res3.size()), flush=True)
 
-
         run = compile_fx(m, example_inputs)
 
+        inductor_res = run(*example_inputs)
+        inductor_res = run(*example_inputs)
+        print("inductor_res.size() is: {}".format(inductor_res.size()), flush=True)
+        input2 = torch.randn(200, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+        inductor_res2 = run(input2)
+        print("inductor_res2.size() is: {}".format(inductor_res2.size()), flush=True)
+        input3 = torch.randn(2, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+        inductor_res3 = run(input3)
+        print("inductor_res3.size() is: {}".format(inductor_res3.size()), flush=True)
+
+def test_fp32_torch_compile():
+    model = models.__dict__["resnet50"](pretrained=True).eval()
+    example_inputs = (torch.randn(116, 3, 224, 224).to(memory_format=torch.channels_last),)
+    with torch.no_grad():
+        import pdb;pdb.set_trace()
+        run = torch.compile(model, fullgraph=True, dynamic=True)
         inductor_res = run(*example_inputs)
         inductor_res = run(*example_inputs)
         print("inductor_res.size() is: {}".format(inductor_res.size()), flush=True)
@@ -114,6 +130,9 @@ def test_int8():
         inductor_res3 = run(input3)
         print("inductor_res3.size() is: {}".format(inductor_res3[0].size()), flush=True)
 
+
 if __name__ == "__main__":
     # test_fp32()
-    test_int8()
+    test_fp32_torch_compile()
+    
+    # test_int8()
