fce196baf7955ccb0971d708894eb74715806ab7
leslie-fang-intel <leslie.fang@intel.com>
2023-04-03 13:46:39 +0800

    Refine testcase for decomp code gen

--- inductor/int8/test_int8_model/test_inductor_int8_conv_relu.py
+++ inductor/int8/test_int8_model/test_inductor_int8_conv_relu.py
@@ -12,13 +12,13 @@ from torch.ao.quantization.backend_config import (
     get_qnnpack_backend_config,
 )
 from torch.ao.quantization.backend_config._qnnpack_pt2e import get_qnnpack_pt2e_backend_config
-from torch.ao.quantization.backend_config._inductor_pt2e import get_inductor_pt2e_backend_config
+from torch.ao.quantization.backend_config._x86_inductor_pt2e import get_x86_inductor_pt2e_backend_config
 from torch.ao.quantization.quantize_fx import prepare_fx, convert_to_reference_fx
 from torch.ao.quantization._quantize_pt2e import prepare_pt2e, convert_pt2e
 from torch.ao.ns.fx.utils import (
     compute_sqnr,
 )
-from torch._inductor.compile_fx import compile_fx, compile_fx_quantization
+from torch._inductor.compile_fx import compile_fx
 
 def test_inductor_int8_relu():
     import copy
@@ -115,82 +115,60 @@ def test_inductor_int8_conv_relu():
             x = self.conv(x)
             return self.relu(x)
 
-    torch.backends.quantized.engine = "x86"
-    # example_inputs = (torch.randn(1, 1, 224, 224),)
-    example_inputs = (torch.randn(1, 3, 16, 16).to(memory_format=torch.channels_last),)
-    m = Mod().eval()
-    # program capture
-    
-    
-    #tracing_mode = "real"
-    tracing_mode = "symbolic"
-    m, guards = torchdynamo.export(
-        m,
-        *copy.deepcopy(example_inputs),
-        aten_graph=True,
-        tracing_mode=tracing_mode,
-    )
+    with torch.no_grad():
+        torch.backends.quantized.engine = "x86"
+        # example_inputs = (torch.randn(1, 1, 224, 224),)
+        example_inputs = (torch.randn(1, 3, 16, 16).to(memory_format=torch.channels_last),)
+        m = Mod().eval()
+        # program capture
+        
+        
+        tracing_mode = "real"
+        #tracing_mode = "symbolic"
+        m, guards = torchdynamo.export(
+            m,
+            *copy.deepcopy(example_inputs),
+            aten_graph=True,
+            tracing_mode=tracing_mode,
+        )
 
-    m = m.eval()
-    print("model after torchdynamo export is: {}".format(m), flush=True)
-    print("guards is: {}".format(guards), flush=True)
+        m = m.eval()
+        print("model after torchdynamo export is: {}".format(m), flush=True)
+        print("guards is: {}".format(guards), flush=True)
 
-    # m(torch.randn(2, 3, 16, 16).to(memory_format=torch.channels_last))
-    # exit(-1)
+        # m(torch.randn(2, 3, 16, 16).to(memory_format=torch.channels_last))
+        # exit(-1)
 
-    backend_config = get_inductor_pt2e_backend_config()
-    qconfig = get_default_qconfig("x86")
-    qconfig_mapping = QConfigMapping().set_global(qconfig)
-    before_fusion_result = m(*example_inputs)
+        backend_config = get_x86_inductor_pt2e_backend_config()
+        qconfig = get_default_qconfig("x86")
+        qconfig_mapping = QConfigMapping().set_global(qconfig)
+        before_fusion_result = m(*example_inputs)
 
-    m = prepare_pt2e(m, qconfig_mapping, example_inputs, backend_config)
-    after_prepare_result = m(*example_inputs)
-    print("model after prepare_pt2e is: {}".format(m), flush=True)
+        m = prepare_pt2e(m, qconfig_mapping, example_inputs, backend_config)
+        after_prepare_result = m(*example_inputs)
+        print("model after prepare_pt2e is: {}".format(m), flush=True)
 
-    print("check the result after prepare: {}".format(
-        torch.allclose(before_fusion_result, after_prepare_result)),
-        flush=True)
-    
-    m = convert_pt2e(m)
-    after_quant_result = m(*example_inputs)
-    print("model after convert_pt2e is: {}".format(m), flush=True)
+        print("check the result after prepare: {}".format(
+            torch.allclose(before_fusion_result, after_prepare_result)),
+            flush=True)
+        
+        m = convert_pt2e(m)
+        after_quant_result = m(*example_inputs)
+        print("model after convert_pt2e is: {}".format(m), flush=True)
 
-    print("check the result after convert: {}".format(
-        torch.allclose(before_fusion_result, after_quant_result, rtol=5e-02, atol=5e-02)),
-        flush=True)
+        print("check the result after convert: {}".format(
+            torch.allclose(before_fusion_result, after_quant_result, rtol=5e-02, atol=5e-02)),
+            flush=True)
 
-    # A few ops in EXIR are not supported. Set nopython=False to make it work
-    # run = torch._dynamo.optimize(compile_fx, nopython=False)(m)
-    # run = compile_fx_quantization(m, example_inputs, pytree_unflatten = True)
-    for pytree_unflatten in [False]:
-        copy_m = copy.deepcopy(m)
-        run = compile_fx_quantization(copy_m, example_inputs, pytree_unflatten = pytree_unflatten)
+        run = compile_fx(m, example_inputs)
 
-        # first run
         print("start the first run", flush=True)
         inductor_result = run(*example_inputs)
 
-
-        # second run
         print("start the second run", flush=True)
         inductor_result = run(*example_inputs)
 
-        # Warm up
-        for i in range(20):
-            inductor_result = run(*example_inputs)
-        # Profile
-        with torch.profiler.profile(on_trace_ready=torch.profiler.tensorboard_trace_handler("./profile_log")) as prof:
-            inductor_result = run(*example_inputs)
-        print(prof.key_averages().table(sort_by="self_cpu_time_total"))
-        # print("inductor second run result is: {}".format(inductor_result), flush=True)
-
-        # np.testing.assert_array_almost_equal(res_ref.cpu().numpy(),
-        # res_quantized.cpu().numpy(), decimal=2)
-        print(type(inductor_result), flush=True)
-        print(type(after_quant_result), flush=True)
-        print(torch.allclose(inductor_result if pytree_unflatten else inductor_result[0], after_quant_result, rtol=0.01, atol=0.01), flush=True)
-    print("Finish the test", flush=True)
-
+        print(torch.allclose(before_fusion_result, inductor_result, rtol=5e-02, atol=5e-02), flush=True)
 
 def test_inductor_int8_conv_relu_v2():
     import copy
@@ -247,6 +225,98 @@ def test_inductor_int8_conv_relu_v2():
     # second run
     inductor_result = run(*example_inputs)
 
+def test_op():
+    class Mod(torch.nn.Module):
+        def __init__(self) -> None:
+            super().__init__()
+            self.conv = torch.nn.Conv2d(
+                # in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1
+                in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1
+            )
+            self.relu = torch.nn.ReLU()
+
+        def forward(self, x):
+            x = self.conv(x)
+            return self.relu(x)
+    
+    m = Mod().eval()
+    weight = m.conv.weight
+    bias = m.conv.bias
+    in_channels = 3
+    out_channels = 16
+    kernel_size = 3
+    stride = (1, 1)
+    padding = (1, 1)
+    dilation = (1, 1)
+    x = torch.randn(1, 3, 16, 16).to(memory_format=torch.channels_last)
+    ref_result = torch.ops.aten.convolution.default(x, weight, bias, stride, padding, dilation, False, padding, 1)
+
+    weight_scales = [0.00141001,
+                     0.00135255,
+                     0.00147836,
+                     0.00141083,
+                     0.00150533,
+                     0.00147467,
+                     0.00145911,
+                     0.00150878,
+                     0.00137985,
+                     0.00150667,
+                     0.00146871,
+                     0.00128621,
+                     0.00146854,
+                     0.0013474,
+                     0.00150807,
+                     0.00150064]
+    weight_scales = torch.DoubleTensor(weight_scales)
+    weight_zero_points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+    weight_zero_point = torch.IntTensor(weight_zero_points)
+    x_scale = 0.05
+    x_zp = 26
+    quant_weight = torch.ops.quantized_decomposed.quantize_per_channel(
+        weight,
+        weight_scales,
+        weight_zero_point,
+        0,
+        -128,
+        127,
+        torch.int8
+        )
+    packed_w, packed_bias = torch.ops.quantized.conv_prepack_cpu_tensor(quant_weight,
+                                                        weight_scales,
+                                                        x.size(),
+                                                        x_scale, # x_scale
+                                                        x_zp, # x_zp
+                                                        bias,
+                                                        stride,
+                                                        padding,
+                                                        dilation,
+                                                        1)
+    result2 = torch.ops.quantized.conv_prepacked_weight(
+        x,
+        weight,
+        bias,
+        packed_w,
+        weight_scales,
+        weight_zero_point,
+        0,
+        packed_bias,
+        stride,
+        padding,
+        dilation,
+        False,
+        padding,
+        1,
+        torch.tensor(x_scale),
+        torch.tensor(x_zp)
+    )
+    
+
+    print("ref_result is: {}".format(ref_result), flush=True)
+    print("result2 is: {}".format(result2), flush=True)
+    print(torch.allclose(ref_result, result2, rtol=5e-02, atol=5e-02), flush=True)
+
 if __name__ == "__main__":
     #test_inductor_int8_relu()
     test_inductor_int8_conv_relu()
+    
+    # exit(0)
