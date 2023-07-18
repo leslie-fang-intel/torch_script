60424e17f9d61b3caa71f691688f32d35c6d9d29
leslie-fang-intel <leslie.fang@intel.com>
2023-07-18 08:47:08 +0800

    change api package name space

--- inductor/int8/test_constant_folding/test_constant_folding.py
+++ inductor/int8/test_constant_folding/test_constant_folding.py
@@ -7,11 +7,14 @@ from torch.ao.quantization._quantize_pt2e import (
     convert_pt2e,
 )
 from torch._inductor.compile_fx import compile_fx
-import torch.ao.quantization._pt2e.quantizer.qnnpack_quantizer as qq
+import torch.ao.quantization.pt2e.quantizer.qnnpack_quantizer as qq
 from torch.ao.quantization._quantize_pt2e import (
     convert_pt2e,
     prepare_pt2e_quantizer,
 )
+from torch._dynamo.utils import counters
+
+counters.clear()
 
 torch._dynamo.config.verbose = True
 torch._inductor.config.trace.enabled = True
@@ -28,17 +31,26 @@ torch.manual_seed(local_seed) # Set PyTorch seed
 np.random.seed(seed=local_seed) # Set Numpy seed
 random.seed(local_seed) # Set the Python seed
 
+
+global_use_bias = False
+global_model_to_channel_last = False
+auto_insert_channel_last = True
+
 class M(torch.nn.Module):
     def __init__(self, bias=True):
         super().__init__()
-        self.conv = nn.Conv2d(3, 6, 2, bias=bias, stride=2, padding=0, dilation=1)
+        if auto_insert_channel_last:
+            self.conv = nn.Conv2d(3, 128, 2, bias=bias, stride=2, padding=0, dilation=1)
+        else:
+            self.conv = nn.Conv2d(3, 6, 2, bias=bias, stride=2, padding=0, dilation=1)
 
     def forward(self, x):
         return self.conv(x)
 
 def test_qnnpack_quantizer():
-    example_inputs = (torch.randn(1, 3, 224, 224).contiguous(memory_format=torch.channels_last),)
-    use_bias = True
+    example_inputs = (torch.randn(1, 3, 8, 8).contiguous(memory_format=torch.channels_last),)
+    
+    use_bias = global_use_bias
     m = M(bias=use_bias).eval()
     
     m_copy = copy.deepcopy(m)
@@ -49,8 +61,8 @@ def test_qnnpack_quantizer():
         aten_graph=True
     )
 
-    import torch.ao.quantization._pt2e.quantizer.x86_inductor_quantizer as xiq
-    from torch.ao.quantization._pt2e.quantizer import X86InductorQuantizer
+    import torch.ao.quantization.pt2e.quantizer.x86_inductor_quantizer as xiq
+    from torch.ao.quantization.pt2e.quantizer import X86InductorQuantizer
     from torch.ao.quantization._quantize_pt2e import prepare_pt2e, convert_pt2e, prepare_pt2e_quantizer
     quantizer = X86InductorQuantizer()
     operator_spec = xiq.get_default_x86_inductor_quantization_config()
@@ -64,9 +76,13 @@ def test_qnnpack_quantizer():
         convert_model = convert_pt2e(prepare_model)
         print("converted model is: {}".format(convert_model), flush=True)
 
+        if global_model_to_channel_last:
+            convert_model = convert_model.to(memory_format=torch.channels_last)
+
         convert_model.eval()
 
         convert_model2 = copy.deepcopy(convert_model)
+
 
         # compiler_model = torch.compile(convert_model)
         # compiler_model = compile_fx(convert_model, example_inputs)
@@ -90,6 +106,8 @@ def test_qnnpack_quantizer():
 
         # self.assertEqual(out_eager[0], out_comp, atol=5e-2, rtol=5e-2)
         print(torch.allclose(out_eager[0], out_comp, atol=5e-2, rtol=5e-2))
+
+        print(counters["inductor"]["pattern_matcher_count"])
 
 if __name__ == "__main__":
     test_qnnpack_quantizer()
