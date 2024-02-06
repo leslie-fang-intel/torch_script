0ab2fa6a080ac41319722a63d29eaf5ca2108b65
leslie-fang-intel <leslie.fang@intel.com>
2024-02-06 13:13:38 +0800

    add test_per_channel_fake_quant_vec for stable performance

--- inductor/int8/quant_code_gen/test_fake_quant_code_gen/test_per_channel_fake_quant_vec.py
+++ inductor/int8/quant_code_gen/test_fake_quant_code_gen/test_per_channel_fake_quant_vec.py
@@ -1,10 +1,15 @@
-# CMD: clear && numactl -C 0-27 -m 0 python test_per_channel_fake_quant_vec.py
+# Test: using iomp and jemalloc
+# CMD: clear && numactl -C 0-3 -m 0 python test_per_channel_fake_quant_vec.py
 import torch
 import torch.ao.quantization.fx._decomposed
 
 def qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype):
-    input = torch.ops.quantized_decomposed.quantize_per_channel(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-    input = torch.ops.quantized_decomposed.dequantize_per_channel(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    input = torch.ops.quantized_decomposed.quantize_per_channel(
+        input, scales, zero_points, axis, quant_min, quant_max, dtype
+    )
+    input = torch.ops.quantized_decomposed.dequantize_per_channel(
+        input, scales, zero_points, axis, quant_min, quant_max, dtype
+    )
     return input
 
 def fq(input, scales, zero_points, axis, quant_min, quant_max, dtype):
@@ -27,41 +32,46 @@ print("Axis: ", axis)
 print("Quant min: ", quant_min)
 print("Quant max: ", quant_max)
 
-import time
-warmup_step = 10
-test_step = 100
+with torch.no_grad():
+    import time
+    warmup_step = 500
+    test_step = 2000
 
-print("qdq:")
-# %timeit qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-for _ in range(warmup_step):
-    qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-qdq_start_time = time.time()
-for _ in range(test_step):
-    qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-qdq_end_time = time.time()
+    print("qdq:")
+    # %timeit qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    for _ in range(warmup_step):
+        qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    qdq_start_time = time.time()
+    for _ in range(test_step):
+        qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    qdq_end_time = time.time()
+    ref_res = qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
 
+    print("fake quant:")
+    # %timeit fq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    for _ in range(warmup_step):
+        fq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    fq_start_time = time.time()
+    for _ in range(test_step):
+        fq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    fq_end_time = time.time()
+    ref_res2 = fq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
 
-print("fake quant:")
-# %timeit fq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-for _ in range(warmup_step):
-    fq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-fq_start_time = time.time()
-for _ in range(test_step):
-    fq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-fq_end_time = time.time()
+    print("compiled qdq:")
+    compiled_qdq = torch.compile(qdq)
+    # warmup
+    for _ in range(warmup_step):
+        compiled_qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    # %timeit compiled_qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    compiled_qdq_start_time = time.time()
+    for _ in range(test_step):
+        compiled_qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
+    compiled_qdq_end_time = time.time()
+    res = compiled_qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
 
+    print("dqd time is: {}".format(qdq_end_time-qdq_start_time), flush=True)  # almost 1.2505955696105957
+    print("fq time is: {}".format(fq_end_time-fq_start_time), flush=True)  # almost 0.6014804840087891
+    print("compiled qdqd time is: {}".format(compiled_qdq_end_time-compiled_qdq_start_time), flush=True) # almost 0.1002035140991211
 
-print("compiled qdq:")
-compiled_qdq = torch.compile(qdq)
-# warmup
-for _ in range(warmup_step):
-    compiled_qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-# %timeit compiled_qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-compiled_qdq_start_time = time.time()
-for _ in range(test_step):
-    compiled_qdq(input, scales, zero_points, axis, quant_min, quant_max, dtype)
-compiled_qdq_end_time = time.time()
-
-print("dqd time is: {}".format(qdq_end_time-qdq_start_time), flush=True)
-print("fq time is: {}".format(fq_end_time-fq_start_time), flush=True)
-print("compiled qdqd time is: {}".format(compiled_qdq_end_time-compiled_qdq_start_time), flush=True)
+    print(torch.allclose(ref_res, res), flush=True)
+    print(torch.allclose(ref_res2, res), flush=True)
