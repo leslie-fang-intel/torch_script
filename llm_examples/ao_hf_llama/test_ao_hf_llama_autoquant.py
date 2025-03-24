986e36fd9d26df9dd3c4de96d535890065310a05
leslie-fang-intel <leslie.fang@intel.com>
2025-03-23 23:41:25 -0700

    apply autoquant to llama

--- llm_examples/ao_hf_llama/test_ao_hf_llama_autoquant.py
+++ llm_examples/ao_hf_llama/test_ao_hf_llama_autoquant.py
@@ -2,20 +2,17 @@ import torch
 from transformers import TorchAoConfig, AutoModelForCausalLM, AutoTokenizer
 from torchao.quantization import int4_weight_only
 from torchao.dtypes import Int4CPULayout
-import torch._inductor.config as config
 
-config.freezing = True
-config.max_autotune = True
 
 with torch.no_grad():
     model_name = "meta-llama/Meta-Llama-3-8B"
 
     ## Test 1: AutoQuant
-    # quantization_config = TorchAoConfig("autoquant", min_sqnr=None)
+    quantization_config = TorchAoConfig("autoquant", min_sqnr=None)
 
     ## Test 2: WOQ INT4
     ## quant_config = int4_weight_only(group_size=128, layout=Int4CPULayout(), set_inductor_config=False)
-    quantization_config = TorchAoConfig(quant_type="int4_weight_only")
+    # quantization_config = TorchAoConfig(quant_type="int4_weight_only")
 
     quantized_model = AutoModelForCausalLM.from_pretrained(
         model_name,
@@ -35,11 +32,10 @@ with torch.no_grad():
     prompt = "Hi, how are you today?"
     inputs = tokenizer(prompt, return_tensors="pt")
     # Warm up run to record shapes for autoquant
+    generate_ids = quantized_model.generate(inputs.input_ids, max_length=100)
+    quantized_model.finalize_autoquant()
 
-    # Optional: apply torch.compile
-    quantized_model.forward = torch.compile(quantized_model.forward)
-
-    generate_ids = quantized_model.generate(inputs.input_ids, max_length=100) 
+    generate_ids = quantized_model.generate(inputs.input_ids, max_length=100)   
     res = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 
     # print("res is: {}".format(res), flush=True)
