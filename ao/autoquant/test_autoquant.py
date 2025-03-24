b71d46ea3588b725fe49f66e4b19c7c084a47916
leslie-fang-intel <leslie.fang@intel.com>
2025-03-24 04:43:23 -0700

    update test_autoquant

--- ao/autoquant/test_autoquant.py
+++ ao/autoquant/test_autoquant.py
@@ -17,7 +17,7 @@ from torchao.quantization import ALL_AUTOQUANT_CLASS_LIST
 # # dynamo_config.inline_inbuilt_nn_modules = False
 
 config.freezing = True
-config.max_autotune = True
+# config.max_autotune = True
 output_channels = 1024
 
 dtype = torch.bfloat16
@@ -32,21 +32,28 @@ class M(torch.nn.Module):
         attn_weights = self.lin(attn_weights)
         return attn_weights
 
-
+manual = True
+manual = False
 if __name__ == "__main__":
     with torch.no_grad():
         model = M(output_channels, dtype).eval()
         ## Optional: invoke torch.compile
         model = torch.compile(model)
-        model = torchao.autoquant(model, manual=True)
+        model = torchao.autoquant(model, manual=manual)
+        # model = torchao.autoquant(model, manual=True)
         # model = torchao.autoquant(model, manual=True, set_inductor_config=False)
         # model = torchao.autoquant(model, set_inductor_config=False)
 
         x = torch.randn(2, 1024).to(dtype)
         print("---- start the formal run ----", flush=True)
         model(x)
-        model.finalize_autoquant()
+        if manual:
+            model.finalize_autoquant()
         print("---- start the second run ----", flush=True)
-        # Do we need to invoke model = torch.compile(model)
         model(x)
+
+        print("---- start the thrid run ----", flush=True)
+        # Both manual = True or False can hit the compile path as _weight_int8pack_mm_cpu
+        res = model(x)
+        
         print("---- finished ----", flush=True)
