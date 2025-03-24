718f8df193a0a3e1d04d3c0badf99ae5b16484e7
leslie-fang-intel <leslie.fang@intel.com>
2025-03-24 01:10:32 -0700

    update

--- ao/autoquant/test_autoquant.py
+++ ao/autoquant/test_autoquant.py
@@ -16,18 +16,8 @@ from torchao.quantization import ALL_AUTOQUANT_CLASS_LIST
 
 # # dynamo_config.inline_inbuilt_nn_modules = False
 
-# config.freezing = True
-# config.freezing_discard_parameters = True
-
-# # config.max_autotune = True
-# # config.max_autotune_gemm_backends = "CPP,"
-# config.fx_graph_cache = False
-# config.fx_graph_remote_cache = False
-# config.autotune_local_cache = False
-
-# dynamic = True
-
-# output_channels = 4096 * 1024
+config.freezing = True
+config.max_autotune = True
 output_channels = 1024
 
 dtype = torch.bfloat16
@@ -37,30 +27,26 @@ class M(torch.nn.Module):
     def __init__(self, output_channels, dtype):
         super().__init__()
         self.lin = torch.nn.Linear(1024, output_channels, bias=False).to(dtype)
-        # self.linear2 = torch.nn.Linear(output_channels, 1024)
 
     def forward(self, attn_weights):
         attn_weights = self.lin(attn_weights)
-        # attn_weights = self.linear2(attn_weights)
         return attn_weights
 
 
 if __name__ == "__main__":
     with torch.no_grad():
         model = M(output_channels, dtype).eval()
-
-        # model = torch.compile(model, mode="max-autotune")
-        # model = autoquant(
-        #     model,
-        #     qtensor_class_list=ALL_AUTOQUANT_CLASS_LIST,
-        #     set_inductor_config=False,
-        #     # **self.quantization_config.quant_type_kwargs,
-        # )
-
         ## Optional: invoke torch.compile
-        # model = torch.compile(model)
-        torchao.autoquant(model, set_inductor_config=False)
+        model = torch.compile(model)
+        model = torchao.autoquant(model, manual=True)
+        # model = torchao.autoquant(model, manual=True, set_inductor_config=False)
+        # model = torchao.autoquant(model, set_inductor_config=False)
 
         x = torch.randn(2, 1024).to(dtype)
+        print("---- start the formal run ----", flush=True)
+        model(x)
+        model.finalize_autoquant()
+        print("---- start the second run ----", flush=True)
+        # Do we need to invoke model = torch.compile(model)
         model(x)
         print("---- finished ----", flush=True)
