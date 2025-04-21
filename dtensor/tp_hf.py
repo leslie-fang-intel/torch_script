f52e4d1077408791f6ba4698550b8b1ca837f256
leslie-fang-intel <leslie.fang@intel.com>
2025-04-21 01:33:05 -0700

    modify tp code

--- dtensor/tp_hf.py
+++ dtensor/tp_hf.py
@@ -27,11 +27,11 @@ print(f"OMP_NUM_THREADS={os.getenv('OMP_NUM_THREADS')}")
 
 model_id = "meta-llama/Llama-3.1-8B-Instruct"
 
-def main(is_tp, rank, world_size) -> None:
+def main(is_tp, rank, world_size, use_compile=False, do_profile=False) -> None:
     backend = "ccl"
     print(is_tp)
     if is_tp:
-        dist.init_process_group(backend)
+        dist.init_process_group(backend, rank=rank, world_size=world_size)
 
     model_kwargs = dict(torch_dtype=torch.bfloat16)
     if is_tp:
@@ -56,24 +56,40 @@ def main(is_tp, rank, world_size) -> None:
     for i in range(5):
         with torch.no_grad():
             outputs = model(inputs)
+            # outputs = model.generate(inputs, do_sample=False, max_new_tokens=128, min_new_tokens=128)
+    
+    if use_compile:
+        with torch.no_grad():
+            model.forward = torch.compile(model.forward)
+            outputs = model(inputs)
+            # outputs = model.generate(inputs, do_sample=False, max_new_tokens=128, min_new_tokens=128)
 
     if is_tp:
         dist.barrier()
-    # profiler = Profiler()
-    # profiler.start()
 
-    # with torch.profiler.profile(
-    #     activities=[
-    #         torch.profiler.ProfilerActivity.CPU,
-    #         torch.profiler.ProfilerActivity.CUDA,
-    #     ],
-    # ) as prof:
     for i in range(5):
         with torch.no_grad():
             start = time.time()
             outputs = model(inputs)
+            # outputs = model.generate(inputs, do_sample=False, max_new_tokens=128, min_new_tokens=128)
             end = time.time()
             print(f"time cost {(end-start)*1000} ms")
+
+
+    if do_profile:
+        with torch.profiler.profile(
+            activities=[
+                torch.profiler.ProfilerActivity.CPU,
+                torch.profiler.ProfilerActivity.CUDA,
+            ],
+        ) as prof:
+            with torch.no_grad():
+                start = time.time()
+                outputs = model(inputs)
+                # outputs = model.generate(inputs, do_sample=False, max_new_tokens=128, min_new_tokens=128)
+                end = time.time()
+                print(f"time cost {(end-start)*1000} ms")
+        print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
     
     # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
 
@@ -103,4 +119,6 @@ if __name__ == "__main__":
     rank = int(os.environ["RANK"]) if "RANK" in os.environ else 0
     world_size = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
     is_tp = "RANK" in os.environ
-    main(is_tp, rank, world_size)
+    use_compile = "USE_COMPILE" in os.environ
+    do_profile = "DO_PROFILE" in os.environ
+    main(is_tp, rank, world_size, use_compile, do_profile)
